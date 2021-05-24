f873743ad8cc4afc904930ff9d38a94574dbfa33
wyw <1464952046@qq.com>
2021-05-24 19:31:20 +0800

    1111

---
+++ src/main/java/com/wyw/springclould/SpringClouldApplication.java
@@ -0,0 +1,13 @@
+package com.wyw.springclould;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClouldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClouldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wyw/springclould/SpringClouldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyw.springclould;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClouldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
